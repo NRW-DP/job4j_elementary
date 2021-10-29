ae901090d118cfc1dd002bce081fc7630d42ae5f
khlap <khlaponin.dp@gmail.com>
2021-10-29 20:23:25 +0200

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new [10];
+        String[] surnames = new[100500];
+        float[] prices = new[40];
+    }
+}
